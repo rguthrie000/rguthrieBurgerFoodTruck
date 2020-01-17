4bb3e4384f6de457b74fd603d0ebd53e13043fe6
rguthrie <rguthrie000@gmail.com>
2020-01-17 15:15:30 -0500

    corrected syntax error in enhanced connection code to support heroku.

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,13 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host:     "localhost",
-  port:     3306,
-  user:     "root",
-  password: "root",
-  database: "burgerFT_db"
-});
+    host:     "localhost",
+    port:     3306,
+    user:     "root",
+    password: "root",
+    database: "burgerFT_db"
+  });
+}
 
 // Connect to our database
 connection.connect((err) => {
